c1f315d7451cc3902d462fca56b5ca311b833187
ndungu27 <158464316+ndungu27@users.noreply.github.com>
2024-05-14 11:53:00 +0000

    created jobitem.html rendered jobs from app.py

--- app.py
+++ app.py
@@ -2,9 +2,37 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+JOBS=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Nairobi, Kenya',
+    'salary':'Ksh. 150000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientiist',
+    'location':'Nairobi, Kenya',
+    'salary':'Ksh. 250000'
+  },
+{
+  'id':3,
+  'title':'Front End Developer',
+  'location':'Arusha, Tanzania'
+},
+  {
+    'id':4,
+    'title':'back end developer',
+    'location':'Remote',
+    'salary':'Ksh. 350000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', 
+                         jobs=JOBS,
+                        company_name='Erick')
 
 
 if __name__ == "__main__":
